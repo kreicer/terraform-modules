e49c1f83d505f8e088e2a84f63b98cd2da2da098
kreicer <listopadovdv@gmail.com>
2024-03-30 16:05:11 +0300

    fix spaces

--- yandex-cloud/examples/vpc_network/main.tf
+++ yandex-cloud/examples/vpc_network/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-    source          = "../../modules/vpc_network"
+  source          = "../../modules/vpc_network"
 
-    folder_id       = "b1guem0u1ksdc9lla7dv"
-    vpc_name        = "default"
-    vpc_desc        = "Default network"
+  folder_id       = "b1guem0u1ksdc9lla7dv"
+  vpc_name        = "default"
+  vpc_desc        = "Default network"
 }
